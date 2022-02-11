d80402d3f2e4d797a84cb9803545b07446052d3c
dikum98 <dikum98@gmail.com>
2022-02-11 17:11:37 +0900

    Two Sum

---
+++ easy__1.js
@@ -0,0 +1,24 @@
+// LeetCode easy - Two Sum
+
+// 164ms 42.4MB
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// 224ms 42.7MB
+var twoSum = function (nums, target) {
+  let answer = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        answer = answer.concat([i, j]);
+      }
+    }
+  }
+};
