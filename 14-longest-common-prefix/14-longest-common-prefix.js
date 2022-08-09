0c77150d31b0f21ecd8b927cfb2c77cd624daa5a
johan <97519893+dikum98@users.noreply.github.com>
2022-08-09 13:51:24 +0900

    Time: 79 ms (77.57%), Space: 42.2 MB (82.53%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,14 @@
+function longestCommonPrefix(strs) {
+  let prefixx = '';
+  if (strs.length === 1) return strs[0];
+  if (strs.includes('')) return '';
+
+  let i = 0;
+  while (i < strs[0].length) {
+    if (strs.some((word) => word[i] !== strs[0][i])) return prefixx;
+    prefixx += strs[0][i];
+    i++;
+  }
+
+  return prefixx;
+}
