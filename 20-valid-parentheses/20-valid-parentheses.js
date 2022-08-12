e3ea6caaf8e98dfe01ee13f863add8efdfd2a6d3
johan <97519893+dikum98@users.noreply.github.com>
2022-08-12 22:16:08 +0900

    Time: 72 ms (85.61%), Space: 42 MB (81.78%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -1,13 +1,16 @@
 function isValid(s) {
   const stack = [s[0]];
-  const opener = ['(', '{', '['];
-  const closer = [')', '}', ']'];
-  if (closer.includes(stack[0])) return false;
+  const parentheses = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+  };
   for (let i = 1; i < s.length; i++) {
-    if (opener.includes(s[i])) stack.push(s[i]);
-    else if (stack[stack.length - 1] === '{' && s[i] === '}') stack.pop();
-    else if (stack[stack.length - 1] === '[' && s[i] === ']') stack.pop();
-    else if (stack[stack.length - 1] === '(' && s[i] === ')') stack.pop();
+    const curr = s[i];
+    // s의 현재 요소가 여는 괄호인 경우
+    if (!parentheses[curr]) stack.push(curr);
+    // 닫는 괄호 짝이 맞는 경우
+    else if (stack[stack.length - 1] === parentheses[curr]) stack.pop();
     else return false;
   }
   return stack.length === 0 ? true : false;
