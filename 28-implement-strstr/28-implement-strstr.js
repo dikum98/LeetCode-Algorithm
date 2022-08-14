9441ba281fbe6a4a5f5c4b73fce9507933e6aa36
johan <97519893+dikum98@users.noreply.github.com>
2022-08-14 17:32:03 +0900

    Time: 67 ms (87.73%), Space: 42.3 MB (24.68%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -1,10 +1,6 @@
-// 28. Implement strStr()
 function strStr(haystack, needle) {
-  // haystack에 needle이 속하는지
-  // needle 길이만큼 haystack 첨부터 검사
-
-  // 속하면 일치하는 인덱스 반환, 아니면 -1반환
-  for (let i = 0; i <= haystack.length - needle.length; i++) {
+  let len = haystack.length - needle.length;
+  for (let i = 0; i <= len; i++) {
     if (haystack.slice(i, i + needle.length) === needle) return i;
   }
   return -1;
