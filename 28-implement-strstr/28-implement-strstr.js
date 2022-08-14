98f0cb41f98be7f0dc188adf0f6cbfd4d690eb29
johan <97519893+dikum98@users.noreply.github.com>
2022-08-14 17:35:56 +0900

    Time: 43 ms (100.00%), Space: 42.1 MB (51.75%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -1,4 +1,4 @@
-function strStr(haystack, needle) {
+const strStr = function (haystack, needle) {
   let len = needle.length;
   for (let i = 0; i < haystack.length; i++) {
     if (haystack.slice(i, i + len) === needle) return i;
