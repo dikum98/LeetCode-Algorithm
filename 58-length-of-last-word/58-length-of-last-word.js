6a9706bafd9105eb4e9c088ac55cb4a9fdc3de4d
johan <97519893+dikum98@users.noreply.github.com>
2022-08-15 17:15:15 +0900

    Time: 105 ms (19.34%), Space: 42.3 MB (19.50%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -2,7 +2,7 @@ const lengthOfLastWord = function (s) {
   let len = 0;
   for (let i = s.length - 1; i >= 0; i--) {
     if (s[i] === ' ') {
-      if (len !== 0) return len;
+      if (len !== 0) break;
       continue;
     }
     len++;
