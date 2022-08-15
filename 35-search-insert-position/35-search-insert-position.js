32c9fbaf536d4fce566aecc21e2edda8102f0b72
johan <97519893+dikum98@users.noreply.github.com>
2022-08-15 14:40:11 +0900

    Time: 97 ms (34.00%), Space: 42.4 MB (25.69%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -1,7 +1,8 @@
 var searchInsert = function (nums, target) {
-  if (nums.indexOf(target) !== -1) return nums.indexOf(target);
+  const indexOfTarget = nums.indexOf(target);
+  if (indexOfTarget !== -1) return indexOfTarget;
+  if (nums[nums.length - 1] < target) return nums.length;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > target) return i;
   }
-  return nums.length;
 };
